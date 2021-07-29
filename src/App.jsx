6e204499b6c4b8ba7265d8c0a8bd15c4ae003c20
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-07-29 16:50:32 +0200

    file set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+const baseURL = "";
+
+
+export default function App() {
+  
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
